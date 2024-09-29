296ed75137dff4b8844dcbe2542231f9486567f9
HaerDalisBlade <HaerDalisBlade@yandex.ru>
2024-09-29 23:23:29 +0300

    major changes 3

--- src/main/java/ru/yandex/practicum/filmorate/validators/ReleaseDate.java
+++ src/main/java/ru/yandex/practicum/filmorate/validators/ReleaseDate.java
@@ -3,7 +3,6 @@ package ru.yandex.practicum.filmorate.validators;
 import jakarta.validation.Constraint;
 import jakarta.validation.Payload;
 
-import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.Target;
 
