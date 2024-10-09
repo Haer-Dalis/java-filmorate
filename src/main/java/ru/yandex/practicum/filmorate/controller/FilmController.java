2a2803e28885cd03ff7803a6ef76b54d89744908
Erensar <knight.hedge@gmail.com>
2024-10-09 21:53:44 +0300

    added error

--- src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import ru.yandex.practicum.filmorate.exception.BadRequestException;
+import ru.yandex.practicum.filmorate.exception.InternalErrorException;
 import ru.yandex.practicum.filmorate.exception.NotFoundException;
 import ru.yandex.practicum.filmorate.model.ErrorResponse;
 import ru.yandex.practicum.filmorate.model.Film;
@@ -71,6 +72,12 @@ public class FilmController {
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public ErrorResponse handle(final BadRequestException e) {
         return new ErrorResponse("Bad request.");
+    }
+
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public ErrorResponse handle(final InternalErrorException e) {
+        return new ErrorResponse("Internal server error.");
     }
 
 }
