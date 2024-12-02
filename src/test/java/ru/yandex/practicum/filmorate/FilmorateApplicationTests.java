f835c5750932efee7df7d1829c92faf854ee9ac4
Erensar <knight.hedge@gmail.com>
2024-12-02 23:31:11 +0300

    db changes

--- src/test/java/ru/yandex/practicum/filmorate/FilmorateApplicationTests.java
+++ src/test/java/ru/yandex/practicum/filmorate/FilmorateApplicationTests.java
@@ -1,18 +1,10 @@
 package ru.yandex.practicum.filmorate;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import ru.yandex.practicum.filmorate.model.User;
 import jakarta.validation.Validation;
 import jakarta.validation.Validator;
 import jakarta.validation.ValidatorFactory;
-import jakarta.validation.ConstraintViolation;
 import org.junit.jupiter.api.BeforeAll;
-import java.time.LocalDate;
-import java.util.Set;
-
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @SpringBootTest
 class FilmorateApplicationTests {
@@ -24,38 +16,5 @@ class FilmorateApplicationTests {
 		ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
 		validator = factory.getValidator();
 	}
-
-	@Test
-	public void testBadEmail() {
-		User user = new User("badEmail",
-				"ErevosX", "Alex", LocalDate.of(2010, 9, 7));
-		Set<ConstraintViolation<User>> violations = validator.validate(user);
-		assertFalse(violations.isEmpty());
-	}
-
-	@Test
-	public void testLoginWithProbely() {
-		User user = new User("good@email.com", "bad login", "Alex",
-				LocalDate.of(2010, 9, 7));
-		Set<ConstraintViolation<User>> violations = validator.validate(user);
-		assertFalse(violations.isEmpty());
-	}
-
-	@Test
-	public void testUser() {
-		User user = new User("hero@email.com",
-				"validLogin", "Alex", LocalDate.of(2010, 9, 7));
-		Set<ConstraintViolation<User>> violations = validator.validate(user);
-		assertTrue(violations.isEmpty());
-	}
-
-	@Test
-	public void testBadBirthday() {
-		User user = new User("good@email.com", "goodLogin", "Max",
-				LocalDate.of(2094, 7, 5));
-		Set<ConstraintViolation<User>> violations = validator.validate(user);
-		assertFalse(violations.isEmpty());
-	}
-
 
 }
